212d91b1e0bfc5da225d1d012295d0925bd7bfce
faridaALmekawi <121612868+faridaALmekawi@users.noreply.github.com>
2022-12-29 14:21:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number__discret_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int y = int.Parse(Console.ReadLine());
+            bool prime =true;
+            Console.WriteLine("the prime number is : ");
+            for (int i =x; i <=y; i++)
+            {
+                for (int j = 2; j < y; j++)
+                {
+                    if (i!=j && i%j==0)
+                    {
+                        prime = false;
+                        break;
+                        }
+                    if (i == 1)
+                        prime = false;
+
+                  }
+                if (prime)
+                    Console.WriteLine(i);
+                prime = true;
+            }
+
+        }
+    }
+}
